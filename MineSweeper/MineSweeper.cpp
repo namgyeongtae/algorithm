49b1d23d35eadc36fb9a94bf65184ef517bcb6b1
namgyeongtae <nrudxo12@kookmin.ac.kr>
2021-09-07 10:34:21 +0900

    second commit

---
+++ MineSweeper/MineSweeper.cpp
@@ -0,0 +1,141 @@
+﻿// MineSweeper.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Pos
+{
+    int _x, _y;
+
+    Pos (int x, int y) : _x(x), _y(y) {}
+
+    enum Dir
+    {
+        LEFT,
+        LEFTUP,
+        UP,
+        RIGHTUP,
+        RIGHT,
+        RIGHTDOWN,
+        DOWN,
+        LEFTDOWN
+    };
+
+    bool sizeCheck(int h, int s)
+    {
+        if (_x < 0 || _x > s - 1 || _y < 0 || _y > h - 1)
+            return false;
+        return true;
+    }
+
+    vector<Pos> saveDir()
+    {
+        vector<Pos> ret;
+
+        for (int i = 0; i < 8; i++)
+        {
+            switch (i)
+            {   
+                case (int) LEFT:
+                    ret.push_back(Pos(_x - 1, _y));
+                    break;
+                case (int) LEFTUP:
+                    ret.push_back(Pos(_x - 1, _y - 1));
+                    break;
+                case (int) UP:
+                    ret.push_back(Pos(_x, _y - 1));
+                    break;
+                case (int)RIGHTUP:
+                    ret.push_back(Pos(_x + 1, _y - 1));
+                    break;
+                case (int)RIGHT:
+                    ret.push_back(Pos(_x + 1, _y));
+                    break;
+                case (int)RIGHTDOWN:
+                    ret.push_back(Pos(_x + 1, _y + 1));
+                    break;
+                case (int)DOWN:
+                    ret.push_back(Pos(_x, _y + 1));
+                    break;
+                case (int)LEFTDOWN:
+                    ret.push_back(Pos(_x - 1, _y + 1));
+                    break;
+            }
+        }
+
+        return ret;
+    }
+};
+
+struct Maze
+{
+    int _h, _s;
+    vector<vector<char>> _mazeVec;
+    Pos start = { 0, 0 };
+
+    Maze (int h, int s) : _h(h), _s(s) {}
+
+    void Init()
+    {
+        vector<char> vec;
+        _mazeVec.assign(_h, vec);
+
+        for (int i = 0; i < _h; i++)
+        {
+            string s;
+            cin >> s;
+            for (int j = 0; j < _s; j++)
+            {
+                _mazeVec[i].push_back(s[j]);
+            }
+        }
+    }
+};
+
+int main()
+{
+    int h, s;
+    cin >> h >> s;
+    int count = 0;
+
+    Maze maze(h, s);
+
+    maze.Init();
+
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < s; j++)
+        {
+            count = 0;
+            maze.start = Pos(j, i);
+            if (maze._mazeVec[i][j] == '.')
+            {
+                vector<Pos> directors = maze.start.saveDir();
+                for (Pos pos : directors)
+                {
+                    if (pos.sizeCheck(h, s))
+                        if (maze._mazeVec[pos._y][pos._x] == '#')
+                            count++;
+                }
+                maze._mazeVec[i][j] = (char) (count + 48);
+            }
+        }
+        
+    }
+
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < s; j++)
+        {
+            cout << maze._mazeVec[i][j];
+        }
+        cout << endl;
+    }
+    
+}
+
+
---
+++ SufflePermutation/SufflePermutation.cpp
@@ -0,0 +1,173 @@
+﻿// SufflePermutation.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define MOD 998244353
+
+using namespace std;
+
+vector<vector<int>> _matrix;
+
+void InitMatrix(int n)
+{
+    int input;
+    vector<int> vec;
+    _matrix.assign(n, vec);
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> input;
+            _matrix[i].push_back(input);
+        }
+    }
+    
+}
+
+struct UnionFind
+{
+    vector<int> pa, rank, size;
+    int _size;
+
+    UnionFind(int n) : rank(n), size(n, 1)
+    {
+        for (int i = 0; i < n; i++)
+            pa.push_back(i);   
+        _size = n;
+    }
+
+    int Find(int x)
+    {
+        if (x == pa[x])
+            return x;
+        else
+            return pa[x] = Find(pa[x]);
+    }
+
+    void Union(int x, int y)
+    {
+        x = Find(x);
+        y = Find(y);
+
+        if (x == y)
+            return;
+
+       
+        
+       if (x < y)
+            pa[y] = x;
+        else
+            pa[x] = y;
+
+        
+    }
+
+    void connectNode()
+    {
+        for (int i = 0; i < _size; i++)
+        {
+            pa[i] = Find(i);
+        }
+    }
+
+};
+
+long long factorial(long long x)
+{
+    if (x == 0) return 1;
+    else if (x == 1) return x;
+    else
+        return x * factorial(x - 1) % MOD;
+}
+
+int main()
+{
+    int N, K;
+    cin >> N >> K;
+
+    InitMatrix(N);
+
+    bool swapable = true;
+
+    UnionFind rowUnion(N);
+    UnionFind colUnion(N);   
+
+    for (int x = 0; x < N; x++)
+    {
+        for (int y = x + 1; y < N; y++)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                
+                if (_matrix[i][x] + _matrix[i][y] > K)
+                {
+                    swapable = false;                   
+                    break;
+                }
+                
+            }
+
+            if (swapable)
+            {
+                colUnion.Union(x, y);
+                colUnion.connectNode();
+            }
+            swapable = true;
+        }
+    }
+    
+
+    for (int x = 0; x < N; x++)
+    {
+        for (int y = x + 1; y < N; y++)
+        {
+            for (int i = 0; i < N; i++)
+            {              
+                if (_matrix[x][i] + _matrix[y][i] > K)
+                {
+                    swapable = false;
+                    break;
+                }              
+            }
+
+            if (swapable)
+            {
+                rowUnion.Union(x, y);
+                rowUnion.connectNode();
+            }
+            swapable = true;
+        }
+    }
+    
+
+    long long res = 1;
+    vector<long long> fact;
+    fact.assign(N, 0);
+
+    for (int i = 0; i < N; i++)
+        fact[colUnion.pa[i]] += 1;
+
+    for (int i = 0; i < N; i++)
+    {
+        res *= factorial(fact[i]);
+        //res *= factorial(colUnion.size[colUnion.Find(i)]);
+    }
+
+    fact.assign(N, 0);
+
+    for (int i = 0; i < N; i++)
+        fact[rowUnion.pa[i]] += 1;
+
+    for (int i = 0; i < N; i++)
+    {
+        res *= factorial(fact[i]);
+        // res *= factorial(rowUnion.size[rowUnion.Find(i)]);
+    }
+
+    cout << res % MOD << endl;
+}
+
+
