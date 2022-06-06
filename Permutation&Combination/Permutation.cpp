00c12d1be475e6f3fd11bca9e523e146130a37db
namgyeongtae <nrudxo12@kookmin.ac.kr>
2022-06-06 18:42:36 +0900

    Create Permutation.cpp
    Create Permutation algorithm

---
+++ Permutation&Combination/Permutation.cpp
@@ -0,0 +1,45 @@
+// PermutationAndCombination.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+char arr[4];
+bool check[4];
+vector<char> v;
+
+void Permutation(int n, int r)
+{
+    if (n == r)
+    {
+        for (int i = 0; i < v.size(); i++)
+        {
+            cout << v[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (check[i]) continue;
+
+        check[i] = true;
+        v.push_back(arr[i]);
+        Permutation(n + 1, r);
+        v.pop_back();
+        check[i] = false;
+    }
+}
+
+int main()
+{
+    arr[0] = 'a';
+    arr[1] = 'b';
+    arr[2] = 'c';
+    arr[3] = 'd';
+
+    Permutation(0, 2);
+}
